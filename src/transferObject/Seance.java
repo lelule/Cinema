c86e5fe175604c10b6151c8078df7e435cff36bd
Ludovic <leluleweb@gmail.com>
2017-12-21 14:14:44 +0100

    Ajout des attibuts,constructeur,getteurs.

--- src/transferObject/Seance.java
+++ src/transferObject/Seance.java
@@ -10,5 +10,31 @@ package transferObject;
  * @author Ludo
  */
 public class Seance {
+    private Film film;
+    private Salle salle;
+    private Jour jour;
+    private Heure heure;
+
+    public Seance(Film film, Salle salle, Jour jour, Heure heure) {
+        this.film = film;
+        this.salle = salle;
+        this.jour = jour;
+        this.heure = heure;
+    }
     
+    public Film getFilm() {
+        return film;
+    }
+
+    public Salle getSalle() {
+        return salle;
+    }
+
+    public Jour getJour() {
+        return jour;
+    }
+
+    public Heure getHeure() {
+        return heure;
+    }
 }
