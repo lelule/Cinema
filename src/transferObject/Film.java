902e58b70dc107a8c9879b30a69fb67c139cffa9
Ludovic <leluleweb@gmail.com>
2017-12-21 14:01:29 +0100

    Ajout de la javadoc a la classe Film

--- src/transferObject/Film.java
+++ src/transferObject/Film.java
@@ -6,14 +6,38 @@
 package transferObject;
 
 /**
- *
+ * Film est la classe represntant un film 
  * @author Ludo
  */
 public class Film {
+    /**
+     * L'ID d'un film. Cette id n'est pas modifiable
+     */
     private int idFilm;
+    /**
+     * Le titre d'un film.
+     */
     private String titre;
+    /**
+     * L'année de sortie du film.
+     */
     private int année;
+    /**
+     * La durée du film exprimé en minutes
+     */
     private int durée;
+    
+    /**
+     * Le constructeur d'un film.
+     * @param idFilm
+     *              L'identifiant du film.
+     * @param titre
+     *              Le titre du film.
+     * @param année
+     *              L'année de sortie du film.
+     * @param durée     
+     *              La durée du film.
+     */
 
     public Film(int idFilm, String titre, int année, int durée) {
         this.idFilm = idFilm;
@@ -21,22 +45,41 @@ public class Film {
         this.année = année;
         this.durée = durée;
     }
-
+    
+    /**
+     * retourne l'id.
+     * @return l'identifiant du film.
+     */
     public int getIdFilm() {
         return idFilm;
     }
-
+    /**
+     * Retourn le titre.
+     * @return le titre du film.
+     */
     public String getTitre() {
         return titre;
     }
-
+    /**
+     * Retourne l'année.
+     * @return l'année de sortie du film.
+     */
     public int getAnnée() {
         return année;
     }
-
+    /**
+     * Retourne la durée.
+     * @return la durée du film.
+     */
     public int getDurée() {
         return durée;
     }
-    
-    
+    /**
+     * Redéfinition de la méthode toString afin d'afficher le titre d'un film
+     * @return le titre d'un film
+     */
+    @Override
+    public String toString() {
+        return titre;
+    }
 }
