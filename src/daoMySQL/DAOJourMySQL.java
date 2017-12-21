5d1803a1e1479c1a35a3143121eee5261fc36fcd
Ludovic <leluleweb@gmail.com>
2017-12-21 14:44:18 +0100

    Ajout de la classe DAOJourMySQL qui implemente DAOJour.

---
+++ src/daoMySQL/DAOJourMySQL.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daoMySQL;
+
+import dao.DAOJour;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import transferObject.Jour;
+
+/**
+ *
+ * @author Ludo
+ */
+public class DAOJourMySQL implements DAOJour{
+    private static DAOJourMySQL uniqueInstance = new DAOJourMySQL();
+
+ public static DAOJourMySQL getInstance()
+ {
+ return uniqueInstance;
+ }
+    
+    @Override
+    public ArrayList<Jour> selectJours(){
+        ArrayList<Jour> listeJours= new ArrayList();
+        String req= "Select * from salle";
+        try {
+            ResultSet resu=ConnexionMySQL.getInstance().selectQuery(req);     
+            while (resu.next()){ 
+                listeJours.add(new Jour(resu.getInt("id_Jour"),resu.getDate("jour")));
+            }
+            
+        }catch(SQLException e){
+            System.out.println(e.toString()+"\nerreur select");
+        }
+        return listeJours;
+    }
+}
